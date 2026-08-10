642ce93f093f3fafb8ebdf4e5b6969f2b0bf704c
saharsalemcs <saharsalemcs@gmail.com>
2026-08-10 22:37:12 +0300

    add Header component and update layout structure; enhance Navigation styling

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,34 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "@/app/_components/Header";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites,surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} min-h-screen antialiased bg-primary-950 text-primary-100 flex flex-col `}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl ">{children}</main>
+        </div>
         <footer>Copyright</footer>
       </body>
     </html>

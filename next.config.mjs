b0524e23977de443f845227b669972ee87b40e40
saharsalemcs <saharsalemcs@gmail.com>
2026-08-11 14:45:56 +0300

    refactor: CabinCard component to use Next.js Image; update page to fetch cabins asynchronously and configure image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zcyjezqakmhhskcmoevi.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

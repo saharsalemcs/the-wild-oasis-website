220180b0d3cde15adc9bcb249a0bf3574606cf35
saharsalemcs <saharsalemcs@gmail.com>
2026-08-16 19:34:47 +0300

    feat: add DateSelector and Reservation components; integrate date selection and reservation form functionality

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,7 +1,15 @@
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
 import TextExpander from "@/app/_components/TextExpander";
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import {
+  getBookedDatesByCabinId,
+  getCabin,
+  getCabins,
+  getSettings,
+} from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
   const { cabinId } = await params;
@@ -22,6 +30,7 @@ export default async function Page({ params }) {
   const { cabinId } = await params;
 
   const cabin = await getCabin(cabinId);
+
   const { name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
@@ -72,9 +81,13 @@ export default async function Page({ params }) {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {name} today. Pay on arrival.
         </h2>
+
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
       </div>
     </div>
   );
